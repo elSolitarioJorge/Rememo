728aadb3adf5f3e3bb9b7cd06282092ab85c8ad8
elSolitarioJorge <Liuhou77@outlook.com>
2026-02-25 13:54:52 +0800

    feat: 集成高德地图并封装生命周期管理

--- app/src/main/java/com/ggg/rememo/RememoApp.java
+++ app/src/main/java/com/ggg/rememo/RememoApp.java
@@ -4,12 +4,17 @@ import android.app.Application;
 
 import com.alibaba.android.arouter.BuildConfig;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.amap.api.maps.MapsInitializer;
 
 
 public class RememoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        MapsInitializer.updatePrivacyShow(this, true, true);
+        MapsInitializer.updatePrivacyAgree(this, true);
+
         if (BuildConfig.DEBUG) {
             ARouter.openLog();
             ARouter.openDebug();
